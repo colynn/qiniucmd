39c954b5e0a06fd68bc1b99f614cc16a95a96d39
colynn <colynnliu@foxmail.com>
2015-07-25 15:33:02 +0000

    initial version

---
+++ README.md
@@ -0,0 +1,2 @@
+# qiniucmd
+qiniucmd is a command line tool for uploading, retrieving and managing data in QiNiu Resoure Storage
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup  
+
+setup(name='qiniucmd',  
+    version = '1.0',  
+    packages = ['qiniu'],
+    scripts = ['qiniucmd'],
+    description = 'Command line tool for managing Qiniu Resource Storage',
+    long_description = 'qiniucmd is a command line tool for uploading,retrieving and managing data in QiNiu Resource Storage.',  
+    author = 'Colynn Liu',
+    author_email = 'colynnliu@foxmail.com',  
+    url = 'http://github.com/colynn',  
+    py_modules = ['qiniucmd'],  
+   )
